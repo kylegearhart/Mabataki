bb6540927719668b9496b780b7d4a5f2d8407d33
kylegearhart <hkgearhart@gmail.com>
2016-03-12 17:55:20 +0900

    Initial Commit

---
+++ Mabataki/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Mabataki
+//
+//  Created by Hunter Kyle Gearhart on 2016/03/12.
+//  Copyright © 2016年 Hunter Kyle Gearhart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+	super.viewDidLoad()
+	// Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+	super.didReceiveMemoryWarning()
+	// Dispose of any resources that can be recreated.
+    }
+
+
+}
