c153a759fd06ac90bf2c5738bf303f4a3fd4eb3d
kylegearhart <hkgearhart@gmail.com>
2016-04-10 19:53:43 +0900

    Add java-spring-backend

---
+++ java-spring-backend/src/main/java/com/kyle/gearhart/MabatakiJavaSpringApplication.java
@@ -0,0 +1,12 @@
+package com.kyle.gearhart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MabatakiJavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MabatakiJavaSpringApplication.class, args);
+	}
+}
---
+++ java-spring-backend/src/test/java/com/kyle/gearhart/MabatakiJavaSpringApplicationTests.java
@@ -0,0 +1,18 @@
+package com.kyle.gearhart;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = MabatakiJavaSpringApplication.class)
+@WebAppConfiguration
+public class MabatakiJavaSpringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
